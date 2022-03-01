a1dc79307029ad232d68709da0931ff5b4c12045
syunta1999 <kaba_mocchan@icloud.com>
2022-03-01 16:24:19 +0900

    UIの向上、編集ファイル=>（HTML、views.py）

--- app/Cross_Word/views.py
+++ app/Cross_Word/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, redirect
-
 # Create your views here
 # クロスワード画面.
 def index(request):
     if request.method == "POST":
-        answer = 'データ'
+        answer = 'OSS'
         word = request.POST['word']
         if answer == word:
             return redirect('cross_word:answer1')
-    return render(request, 'cross_word.html')
+    return render(request, 'cross_word/cross_word.html')
 
 def answer1(request):
-    return render(request, 'answer1.html')
+    params = {
+        'ans': 'OSS(Open Source Software)',
+    }
+    return render(request, 'cross_word/answer1.html', params)
+
+
+
+# def index(request):
+#     params = {
+#         # 'select':QuizSelect(),
+#         'form':AnswerForm(),
+#     }
+#     return render(request,'quiz/index.html',params)
