b4518c235a39e66140bf5fc6a705989cd244246e
syunta1999 <kaba_mocchan@icloud.com>
2022-03-04 12:58:17 +0900

    ヒントテーブルの作成、マイグレーション、UI（入力可能文字数）、日本語の格納ができないという問題あり

--- app/Cross_Word/models.py
+++ app/Cross_Word/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class VerticalHint(models.Model):
+    
+    crossword_id = models.IntegerField(default=1)
+    hint = models.CharField(max_length=500)
+
+    def __str__(self):
+        cross_word = str(self.crossword_id)
+        return "Quiz" + cross_word
+
+class HorizonalHint(models.Model):
+    
+    crossword_id = models.IntegerField(default=1)
+    hint = models.CharField(max_length=500)
+
+    def __str__(self):
+        cross_word = str(self.crossword_id)
+        return "Quiz" + cross_word
+
--- app/Cross_Word/views.py
+++ app/Cross_Word/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, redirect
+from .models import HorizonalHint, VerticalHint
 # Create your views here
 # クロスワード画面.
 def index(request):
+    params = {
+        'vertical':[],
+        'horizonal':[],
+    }
+    params['vertical']=VerticalHint.objects.filter(crossword_id=1)
+    params['horizonal']=HorizonalHint.objects.filter(crossword_id=1)
     if request.method == "POST":
         answer = 'OSS'
         word = request.POST['word']
         if answer == word:
             return redirect('cross_word:answer1')
-    return render(request, 'cross_word/cross_word.html')
+    return render(request, 'cross_word/cross_word.html', params)
 
 def answer1(request):
     params = {
         'ans': 'OSS(Open Source Software)',
+        'vertical':[],
+        'horizonal':[],
     }
+    params['vertical']=VerticalHint.objects.filter(crossword_id=1)
+    params['horizonal']=HorizonalHint.objects.filter(crossword_id=1)
     return render(request, 'cross_word/answer1.html', params)
 
 
