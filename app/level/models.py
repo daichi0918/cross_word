795338e53784c85306ec985d2fa50c247fc5228e
daichi0918 <kayasugadaichi@icloud.com>
2022-03-06 22:57:14 +0900

    commit

--- app/level/models.py
+++ app/level/models.py
@@ -3,15 +3,11 @@ from django.db import models
 # Create your models here.
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 1927875... Merge pull request #11 from daichi0918/mysql_jp
 class QuestionAll(models.Model):
 
     question_id = models.IntegerField()
     # 望月さんが作られた問題集のIDをここで取ってくる
-<<<<<<< HEAD
     # gaibu_key = models.ForeignKey()
 
 
@@ -26,6 +22,3 @@ class Friend(models.Model):
     # def __str__(self):
     #     return '<Friend:id=' + str(self.id) + ', ' + \
 
-=======
-    # gaibu_key = models.ForeignKey()
->>>>>>> parent of 1927875... Merge pull request #11 from daichi0918/mysql_jp
