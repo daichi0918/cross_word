a176564dceb68aaa763f951d425677292582f5ae
rina nishi <rina-ns@leo.bbiq.jp>
2022-03-11 00:40:56 +0900

    データ取得途中

--- app/level/models.py
+++ app/level/models.py
@@ -4,12 +4,31 @@ from django.db import models
 
 
 class QuestionAll(models.Model):
-
+    title = models.CharField(max_length=100)
+    memo = models.TextField()
     question_id = models.IntegerField()
-    # 望月さんが作られた問題集のIDをここで取ってくる
+    # 望月さんが作られた問題集のIDをここで取ってくる?
     # gaibu_key = models.ForeignKey()
+    def __str__(self):
+        return self.question_id
 
+    def get_title(self):
+        try:
+            return self.title
+        except:
+            return False
 
+    def get_memo(self):
+        try:
+            return self.memo
+        except:
+            return False
+
+    def get_question_id(self):
+        try:
+            return self.question_id
+        except:
+            return False
 
 class Friend(models.Model):
     name = models.CharField(max_length=100)
--- app/level/views.py
+++ app/level/views.py
@@ -1,6 +1,8 @@
 from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
+from level.models import QuestionAll
+# from app.models import User # モデルをインポート
 
 # Create your views here.
 def index(request):
@@ -21,3 +23,14 @@ def nomal(request):
 def hard(request):
     # return render(request, 'hard.html')
     return render(request, 'level/hard.html')
+
+# def users(request):
+#     user_list = User.objects.all() # 値一覧を取得
+#     user_dict = {'users': user_list} # ディクショナリーの形で設定
+
+#     return render(request, 'app/users.html', context=user_dict) # context=user_dictとして、users.htmlに渡す
+
+def list(request):
+    question_list = QuestionAll.objects.all()
+    # list_dict = {'question': list1}
+    return render(request, 'level/easy.html', {'question':question_list})
