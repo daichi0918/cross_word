13b756949de9916d14047cf8f3e00e3f015570df
syunta1999 <kaba_mocchan@icloud.com>
2022-03-13 17:31:19 +0900

    ジャヴァスクリプトの佐久市江

---
+++ app/cross_word/static/js/easy/answer1.js
@@ -0,0 +1,33 @@
+
+window.onload = function() {
+    // alert("dddddddddddd");
+// 空白は全角で空ける
+    document.getElementById("one").textContent="A";
+    document.getElementById("one-s").textContent="(1)";
+
+    document.getElementById("two").textContent="B";
+    document.getElementById("two-s").textContent="(2)";
+
+    document.getElementById("three").textContent="　";
+    document.getElementById("three-s").textContent="　";
+
+    document.getElementById("four").textContent="　";
+    document.getElementById("four-s").textContent="　";
+
+    document.getElementById("five").textContent="　";
+    document.getElementById("five-s").textContent="　";
+
+    document.getElementById("six").textContent="　";
+    document.getElementById("six-s").textContent="　";
+
+    document.getElementById("seven").textContent="C";
+    document.getElementById("seven-s").textContent="　";
+
+    document.getElementById("eight").textContent="　";
+    document.getElementById("eight-s").textContent="(3)";
+
+    document.getElementById("nine").textContent="　";
+    document.getElementById("nine-s").textContent="　";
+
+};
+
